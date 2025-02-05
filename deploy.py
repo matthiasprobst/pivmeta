4dbca5c369e12209e732a09a965967e4f084e3b4
MatthiasProbst <matth.probst@gmail.com>
2025-02-05 16:39:46 +0100

    update doi

--- deploy.py
+++ deploy.py
@@ -225,7 +225,7 @@ if __name__ == "__main__":
     prev_version_string = 'v1.1.2'
     version_string = 'v2.0.0'
     img_version_string = 'v1.0.0'
-    doi_url = "https://doi.org/10.5281/zenodo.14740584"
+    doi_url = "https://doi.org/10.5281/zenodo.14811501"
     overwrite = True
 
     version_dir = __this_dir__ / 'docs' / version_string.strip('v')
